f2023b3a09caa1c170befc518f49ae4ad3e781b0
boris86msk <boris86msk@yandex.ru>
2024-03-30 20:25:42 +0300

    init commit

---
+++ src/main/java/ru/openschool/timetrackingsystems/TimeTrackingSystemsApplication.java
@@ -0,0 +1,13 @@
+package ru.openschool.timetrackingsystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackingSystemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeTrackingSystemsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/openschool/timetrackingsystems/TimeTrackingSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.openschool.timetrackingsystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackingSystemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
